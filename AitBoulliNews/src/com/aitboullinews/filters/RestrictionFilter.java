06afc4e6d00cc1894ea7333bcd4233fcc59af26d
you62740 <younes.elouahmani0@gmail.com>
2017-03-08 11:46:08 +0100

    Filtres - Espace Membre

---
+++ AitBoulliNews/src/com/aitboullinews/filters/RestrictionFilter.java
@@ -0,0 +1,48 @@
+package com.aitboullinews.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class RestrictionFilter implements Filter {
+	
+	public void init( FilterConfig config ) throws ServletException {
+
+    }
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		/* Cast des objets request et response */
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        /* Récupération de la session depuis la requête */
+        HttpSession session = request.getSession();
+
+        /**
+         * Si l'objet utilisateur n'existe pas dans la session en cours, alors
+         * l'utilisateur n'est pas connecté.
+         */
+        if ( session.getAttribute( "sessionUtilisateur" ) == null ) {
+            /* Redirection vers la page publique */
+            response.sendRedirect( request.getContextPath() + "/accesPublic.jsp" );
+        } else {
+            /* Affichage de la page restreinte */
+            chain.doFilter( request, response );
+        }
+	}
+	
+	public void destroy() {
+
+    }
+
+}
